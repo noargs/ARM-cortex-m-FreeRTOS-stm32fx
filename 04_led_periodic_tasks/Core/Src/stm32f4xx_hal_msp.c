7941246b4d88d0683657936f5df78f0a62ac23ca
Ibn Asghar <asim.ibn.asghar@gmail.com>
2024-02-28 00:15:56 +0000

    omit handlers, 4 bit nvic prio., tim6 for hal timebase

--- 04_led_periodic_tasks/Core/Src/stm32f4xx_hal_msp.c
+++ 04_led_periodic_tasks/Core/Src/stm32f4xx_hal_msp.c
@@ -22,7 +22,7 @@
 #include "main.h"
 
 /* USER CODE BEGIN Includes */
-
+#include "FreeRTOS.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -70,12 +70,10 @@ void HAL_MspInit(void)
   __HAL_RCC_SYSCFG_CLK_ENABLE();
   __HAL_RCC_PWR_CLK_ENABLE();
 
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
-
   /* System interrupt init*/
 
   /* USER CODE BEGIN MspInit 1 */
-
+  vInitPrioGroupValue();
   /* USER CODE END MspInit 1 */
 }
 
